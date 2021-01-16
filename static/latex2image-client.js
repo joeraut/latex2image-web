9d44282f815a005e71674a16bd79e6db755e85ea
Joseph Rautenbach <joeraut@users.noreply.github.com>
2021-01-16 01:05:01 +0100

    Updated static site with HTML download-attribute on button & JS formatting

--- static/latex2image-client.js
+++ static/latex2image-client.js
@@ -2,74 +2,74 @@ var sampleEquation = '\\frac{\\pi}{2} = \\int_{-1}^{1} \\sqrt{1-x^2}\\ dx';
 var hasShownBefore = false;
 
 $(document).ready(function() {
-    function show(resultData) {
-        function afterSlideUp() {
-            var resultDataJSON;
-            if ((resultDataJSON = JSON.parse(resultData)) && !resultDataJSON.error) {
-                $('#resultImage').attr('src', resultDataJSON.imageURL);
-                $('#downloadButton').attr('href', resultDataJSON.imageURL);
-                $('#resultCard').show();
-                $('#errorAlert').hide();
-            } else {
-                $('#errorAlert').text(resultDataJSON.error || 'Invalid response received');
-                $('#errorAlert').show();
-                $('#resultCard').hide();
+  function show(resultData) {
+    function afterSlideUp() {
+      var resultDataJSON;
+      if ((resultDataJSON = JSON.parse(resultData)) && !resultDataJSON.error) {
+        $('#resultImage').attr('src', resultDataJSON.imageURL);
+        $('#downloadButton').attr('href', resultDataJSON.imageURL);
+        $('#resultCard').show();
+        $('#errorAlert').hide();
+      } else {
+        $('#errorAlert').text(resultDataJSON.error || 'Invalid response received');
+        $('#errorAlert').show();
+        $('#resultCard').hide();
 
-            }
-            $('#result').slideDown(330);
+      }
+      $('#result').slideDown(330);
 
-            // Scroll window to bottom
-            $("html, body").animate({
-                scrollTop: $(document).height()
-            }, 1000);
+      // Scroll window to bottom
+      $("html, body").animate({
+        scrollTop: $(document).height()
+      }, 1000);
 
-            hasShownBefore = true;
-        }
-
-        $('#result').slideUp(hasShownBefore ? 330 : 0, afterSlideUp);
+      hasShownBefore = true;
     }
 
-    $('#convertButton').click(function() {
-        if (!$('#latexInputTextArea').val()) {
-            show(JSON.stringify({
-                error: 'No LaTeX input provided'
-            }));
-            return;
-        }
-        
-        $('#result').slideUp(hasShownBefore ? 330 : 0, function() {
-            $('#resultImage').attr('src', '');
-        });
-        
-        $('#convertButton').prop('disabled', true);
-        $('#exampleButton').prop('disabled', true);
-        $('#convertButton').prop('value', 'Converting...');
-        $.ajax({
-            url: '/convert',
-            type: 'POST',
-            data: {
-                latexInput: $('#latexInputTextArea').val(),
-                outputFormat: $('#outputFormatSelect').val(),
-                outputScale: $('#outputScaleSelect').val()
-            },
-            success: function(data) {
-                $('#convertButton').prop('disabled', false);
-                $('#exampleButton').prop('disabled', false);
-                $('#convertButton').prop('value', 'Convert');
-                show(data);
-            },
-            error: function() {
-                $('#convertButton').prop('disabled', false);
-                $('#exampleButton').prop('disabled', false);
-                $('#convertButton').prop('value', 'Convert');
-                alert('Error communicating with server');
-            }
-        });
+    $('#result').slideUp(hasShownBefore ? 330 : 0, afterSlideUp);
+  }
+
+  $('#convertButton').click(function() {
+    if (!$('#latexInputTextArea').val()) {
+      show(JSON.stringify({
+        error: 'No LaTeX input provided'
+      }));
+      return;
+    }
+
+    $('#result').slideUp(hasShownBefore ? 330 : 0, function() {
+      $('#resultImage').attr('src', '');
     });
 
-    // Show and convert a sample equation
-    $('#exampleButton').click(function() {
-        $('#latexInputTextArea').val(sampleEquation);
-        $('#convertButton').click();
+    $('#convertButton').prop('disabled', true);
+    $('#exampleButton').prop('disabled', true);
+    $('#convertButton').prop('value', 'Converting...');
+    $.ajax({
+      url: '/convert',
+      type: 'POST',
+      data: {
+        latexInput: $('#latexInputTextArea').val(),
+        outputFormat: $('#outputFormatSelect').val(),
+        outputScale: $('#outputScaleSelect').val()
+      },
+      success: function(data) {
+        $('#convertButton').prop('disabled', false);
+        $('#exampleButton').prop('disabled', false);
+        $('#convertButton').prop('value', 'Convert');
+        show(data);
+      },
+      error: function() {
+        $('#convertButton').prop('disabled', false);
+        $('#exampleButton').prop('disabled', false);
+        $('#convertButton').prop('value', 'Convert');
+        alert('Error communicating with server');
+      }
     });
+  });
+
+  // Show and convert a sample equation
+  $('#exampleButton').click(function() {
+    $('#latexInputTextArea').val(sampleEquation);
+    $('#convertButton').click();
+  });
 });
